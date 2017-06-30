eab9c2ed1793f7b44fd1db8b7acee28894bf6245
AnhPham <anh.phamdoduc@nashtechglobal.com>
2017-06-30 17:05:03 +0100

    add new string extension

---
+++ consoleApp/consoleApp/Extension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consoleApp
+{
+    class Extension
+    {
+        public static string Encode() {
+            return string.Empty;
+        }
+    }
+}
