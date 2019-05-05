afa49f169767ba403b376f7f0b5d6676af0f5c6c
ducanh.jp <ducanh.jp@gmail.com>
2019-05-05 23:17:39 +0700

    add codewar folder

---
+++ consoleApp/consoleApp/CodeWar/Solutions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consoleApp.CodeWar
+{
+    public static class Solutions
+    {
+        public static bool IsTrue(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return true;
+
+            str = str.ToUpper();
+            var strCharacter = new List<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (strCharacter.Contains(str[i]))
+                    return false;
+
+                strCharacter.Add(str[i]);
+            }
+
+            return true;
+        }
+    }
+}
